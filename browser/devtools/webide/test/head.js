b1000175da12a56dfa38909479eeaf6c7c6cd924
Paul Rouget <me@paulrouget.com>
2014-10-28 08:51:06 +0100

    Bug 1056923 - Add WebIDE launch button. r=jaws,r=jryans

--- browser/devtools/webide/test/head.js
+++ browser/devtools/webide/test/head.js
@@ -64,6 +64,8 @@ function closeWebIDE(win) {
 
   let deferred = promise.defer();
 
+  Services.prefs.setBoolPref("devtools.webide.widget.enabled", false);
+
   win.addEventListener("unload", function onUnload() {
     win.removeEventListener("unload", onUnload);
     info("WebIDE closed");
